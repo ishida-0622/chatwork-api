2ff3fb8bb8ac17c8d7ae05ccc48651f7ba920451
kentaro ishida <ken.babuo@gmail.com>
2024-04-10 23:13:52 +0900

    退勤リマインダー

---
+++ closing-reminder/index.mjs
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+import { URLSearchParams } from "url";
+
+dotenv.config();
+
+const CHATWORK_API_TOKEN = process.env.CHATWORK_API_TOKEN;
+const ROOM_ID = process.env.CHATWORK_ROOM_ID;
+const TO_USER_ID = process.env.CHATWORK_TO_USER_ID;
+
+export const handler = async (event) => {
+  const encodedParams = new URLSearchParams();
+  encodedParams.set("body", `[To:${TO_USER_ID}]退勤押した？`);
+  encodedParams.set("self_unread", "0");
+
+  const url = `https://api.chatwork.com/v2/rooms/${ROOM_ID}/messages`;
+
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/x-www-form-urlencoded",
+      "x-chatworktoken": CHATWORK_API_TOKEN
+    },
+    body: encodedParams
+  };
+
+  const res = await fetch(url, options);
+  const json = await res.json()
+  console.log(json);
+}
+
+// ローカル実行用
+// handler();
